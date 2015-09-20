459d53acf530af7adf820a99ba27436cf3caf3ac
paulhoughton <paulhoughton@users.noreply.github.com>
2015-09-20 08:40:10 +0100

    Stateless function component

--- src/components/About.js
+++ src/components/About.js
@@ -1,38 +1,29 @@
-import React, { Component, PropTypes } from "react";
+import React from "react";
 
-export default class About extends Component {
-	
-	static propTypes = {
-		ok: PropTypes.func.isRequired
-	};
-	
-	render() {
-		const center = {
-			position: "absolute",
-			top: "30%",
-			left: "50%",
-			margin: "0 0 0 -165px",
-			textAlign: "center"
-		};
-		return (
-			<section className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-				<div className="mdl-card mdl-shadow--2dp" style={center}>
-					<div className="mdl-card__title mdl-card--expand">
-						<h2 className="mdl-card__title-text">Remember</h2>
-					</div>
-					<div className="mdl-card__supporting-text">
-						<h6>Version 1.0</h6>
-						<a href="https://github.com/paulhoughton/remember">Github</a>
-					</div>
-					<div className="mdl-card__actions mdl-card--border">
-						<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-							onClick={this.props.ok}>
-								OK
-						</a>
-					</div>
-				</div>
-			</section>
-		);
-	}
-}
+export default ({ok}) => (
+	<section className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+		<div className="mdl-card mdl-shadow--2dp"
+			style={{
+				position: "absolute",
+				top: "30%",
+				left: "50%",
+				margin: "0 0 0 -165px",
+				textAlign: "center"
+			}}>
+			<div className="mdl-card__title mdl-card--expand">
+				<h2 className="mdl-card__title-text">Remember</h2>
+			</div>
+			<div className="mdl-card__supporting-text">
+				<h6>Version 1.0</h6>
+				<a href="https://github.com/paulhoughton/remember">Github</a>
+			</div>
+			<div className="mdl-card__actions mdl-card--border">
+				<a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+					onClick={ok}>
+						OK
+				</a>
+			</div>
+		</div>
+	</section>
+);
 
