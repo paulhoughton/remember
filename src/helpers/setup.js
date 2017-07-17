490e83f03b7120508122a39fb9a9e4123891a66f
paulhoughton <paulhoughton@users.noreply.github.com>
2017-07-17 08:11:01 +0100

    Ignore geolocation timeouts

--- src/helpers/setup.js
+++ src/helpers/setup.js
@@ -1,25 +1,35 @@
 export function geoListeners(callback) {
   let prev = null;
 
-  navigator.geolocation.watchPosition(({ coords, timestamp }) => callback({
-    latitude: coords.latitude,
-    longitude: coords.longitude,
-    lastUpdate: timestamp,
-    accuracy: Math.floor(coords.accuracy)
-  }),
-  (err) => alert('Unable to find position - ' + err.message),
+  navigator.geolocation.watchPosition(
+    ({ coords, timestamp }) =>
+      callback({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        lastUpdate: timestamp,
+        accuracy: Math.floor(coords.accuracy)
+      }),
+    err => {
+      if (err.code !== 3) {
+        alert('Unable to find position - ' + err.message);
+      }
+    },
     {
       enableHighAccuracy: true,
       timeout: 15000
     }
   );
 
-  window.addEventListener('deviceorientation', (event) => {
-    if (event.alpha === null) return;
-    const alpha = Math.floor(event.alpha);
-    if (alpha !== prev) {
-      callback({ orientation: alpha });
-      prev = alpha;
-    }
-  }, true);
+  window.addEventListener(
+    'deviceorientation',
+    event => {
+      if (event.alpha === null) return;
+      const alpha = Math.floor(event.alpha);
+      if (alpha !== prev) {
+        callback({ orientation: alpha });
+        prev = alpha;
+      }
+    },
+    true
+  );
 }
